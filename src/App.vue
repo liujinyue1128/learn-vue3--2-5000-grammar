61c0183b0aa1f8155b7d659fa320c3aa28894722
liujinyue <242495772@qq.com>
2024-12-27 16:24:19 +0800

    feat: vue3--变量命名模式

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,14 @@
 
 <script>
 import { ref } from 'vue'
+
+/**
+ * 计数器自定义组合式API
+ * 可以单独封装起来
+ * @returns 返回包含计数器的引用、增加和减少计数的方法的对象
+ */
 const useCount = () =>{
-  let countRef = ref(0);
+  let countRef = ref(0); // 命名格式必有Ref结尾
   const increase = () => {
     countRef.value++
   }
@@ -17,8 +23,10 @@ const useCount = () =>{
 }
  export default {
   setup(){
-    // 返回的所有属性都会暴露给实例模板使用
+    // 只要是在setup中，countRef就是一个对象
+    // 实例代理中，countRef就是countRef.value
     return {
+      // 返回的所有属性都会暴露给实例模板使用
       ...useCount()
     }
   }
