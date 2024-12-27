71082158e8d62eeddaf3047f6841494c82d8c433
liujinyue <242495772@qq.com>
2024-12-27 16:17:02 +0800

    切换分支

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="increase">{{ countRef }}</button>
+  <button @click="decrease">{{ countRef }}</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+const useCount = () =>{
+  let countRef = ref(0);
+  const increase = () => {
+    countRef.value++
+  }
+  const decrease = () => {
+    countRef.value--
+  }
+  return {countRef,increase,decrease}
+}
+ export default {
+  setup(){
+    // 返回的所有属性都会暴露给实例模板使用
+    return {
+      ...useCount()
+    }
+  }
+ }
+</script>
